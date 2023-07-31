3b1059bf11ab1736b7c6209e59c53dcd6d61e798
Nikita Zolotoverkh <example@gmail.com>
2023-07-31 14:50:01 +0300

    changed background, removed advertising, generated list of movies watched

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ const movieDB = {
     ]
 };
 
+
+const promo__adv = document.querySelectorAll('.promo__adv img'),
+    promo__genre = document.querySelector('.promo__genre'),
+    promo__bg = document.querySelector('.promo__bg'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+
+promo__adv.forEach(element => {
+    element.remove();
+})
+promo__genre.textContent = 'ДРАМА';
+promo__bg.style.background = 'url("/img/bg.jpg")'
+movieList.innerHTML = '';
+movieDB.movies.sort();
+movieDB.movies.forEach((movie, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i+1} ${movie}
+            <div class="delete"></div>
+        </li>
+    `;
+});
